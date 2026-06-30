be533fe783e53b1c1bb04bf87ae9bee8e0156387
이서진 <seojin-l>
2026-06-30 21:41:13 +0900

    초기세팅

---
+++ src/main/java/com/example/loopa/LoopaApplication.java
@@ -0,0 +1,13 @@
+package com.example.loopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoopaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loopa/LoopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loopa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
