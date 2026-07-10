b1ce551d1845724d0d516b3c76aa0127b3de3d62
lee-mark01 <seungheelee1122@gmail.com>
2026-07-11 03:52:19 +0900

    user 도메인 dto 수정

--- src/main/java/com/example/loopa/domain/user/dto/response/UserMeResponse.java
+++ src/main/java/com/example/loopa/domain/user/dto/response/UserMeResponse.java
@@ -4,22 +4,26 @@ import com.example.loopa.domain.user.entity.Gender;
 import com.example.loopa.domain.user.entity.Job;
 import com.example.loopa.domain.user.entity.User;
 
+import java.time.LocalDateTime;
+
 public record UserMeResponse(
-        Long id,
+        Long userId,
         String email,
+        Integer tokenBalance,
         Gender gender,
         Integer age,
         Job job,
-        Integer tokenBalance
+        LocalDateTime createdAt
 ) {
     public static UserMeResponse from(User user) {
         return new UserMeResponse(
                 user.getId(),
                 user.getEmail(),
+                user.getTokenBalance(),
                 user.getGender(),
                 user.getAge(),
                 user.getJob(),
-                user.getTokenBalance()
+                user.getCreatedAt()
         );
     }
 }
